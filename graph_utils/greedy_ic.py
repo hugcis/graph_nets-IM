950022493b28a76d3f18c5f3d06faa5d24ac407d
hugcis <hmj.cisneros@gmail.com>
2018-12-17 10:16:33 +0100

    Remove unnecessary files and add models.

--- graph_utils/greedy_ic.py
+++ graph_utils/greedy_ic.py
@@ -1,9 +1,10 @@
-from graph_utils.utils import in_neighbors, pp
+from tqdm import tqdm as base_tqdm
+from graph_utils.utils import in_neighbors, pp, miia
 
 def hsh(i, lst):
     return int( str(hash(repr(lst))) + str(hash(i)))
     
-def ap(u, S, miia_v_theta, grph, cache={}):
+def ap(u, S, miia_v_theta, grph, cache={}, cnt=0):
     """ Compute activation probability of node 
     u, from set S and maximum influence in-arboresence
     miia_v_theta.
@@ -16,10 +17,33 @@ def ap(u, S, miia_v_theta, grph, cache={}):
     else:
         base = 1
         for in_neighbor in n_in:
-            if hsh(in_neighbor,miia_v_theta) in cache:
-                p = cache[hsh(in_neighbor,miia_v_theta)]
-            else:
-                p =  ap(in_neighbor, S, miia_v_theta, grph, cache)
-                cache[hsh(in_neighbor,miia_v_theta)] = p
+            #if hsh(in_neighbor, miia_v_theta) in cache:
+            #    p = cache[hsh(in_neighbor, miia_v_theta)]
+            #else:
+            p =  ap(in_neighbor, S, miia_v_theta, grph, cache)
+            #    cache[hsh(in_neighbor, miia_v_theta)] = p
             base *= (1 - p*pp(in_neighbor, u, grph))
         return 1 - base
+    
+def naive_greedy_algorithm(n_source, grph, tqdm_function=base_tqdm):
+    s = []
+
+    for _ in tqdm_function(range(n_source)):
+        max_influence = 0
+        max_node = 0
+        shared_cache = {}
+        for node in tqdm_function(range(grph.number_of_nodes())):
+            if not node in s:
+                influence = sum(
+                    ap(i, s + [node], 
+                       miia(i, 0.001, grph), 
+                       grph, 
+                       cache=shared_cache) 
+                    for i in range(grph.number_of_nodes()))
+        
+                if influence > max_influence:
+                    max_node = node
+                    max_influence = influence
+        s.append(max_node)
+    
+    return s
